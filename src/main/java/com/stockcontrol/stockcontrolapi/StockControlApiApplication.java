a1dac49803225ce729a979cbc67cd7c44f1a39ff
Isamara Costa <jcisamara@gmail.com>
2026-07-22 11:17:03 -0300

    chore: adiciona estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/stockcontrol/stockcontrolapi/StockControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.stockcontrol.stockcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockControlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockControlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stockcontrol/stockcontrolapi/StockControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockcontrol.stockcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockControlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
